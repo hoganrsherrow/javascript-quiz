07cca2c57c55c8b5f1a9e87ce52a691b31f99413
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-21 22:42:26 -0500

    timer added, but it is a mess

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
-console.log("This works.");
+// Score reset
 var score = 0;
+// Grab the Start Quiz button by id
+var startQuizButton = document.getElementById("start-quiz");
+// Grab div to hold quiz information
+var quizHolder = document.getElementById("questions");
+// start quiz event listener
+function startQuiz() {
+    
+}
+// Create timer
+function addTimerEl() {
+    var timerEl = document.createElement("div");
+    timerEl.classList.add("timer");
+    quizHolder.appendChild(timerEl);
+
+
+    setInterval(updateTimer, 1000)
+}
+// update timer
+function updateTimer() {
+    var time = 300;
+    var minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    let timerEl = document.getElementsByClassName("timer");
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timerEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
+// This array will hold the questions and answers
 questions = [
     {
         question: "Is a Javasript a coding language?",
@@ -19,11 +48,6 @@ questions = [
     }
 
 ]
+startQuizButton.addEventListener("click", addTimerEl);
 
-console.log(questions[1].question);
-console.log(questions[0].answer);
-console.log(score);
-score++;
-console.log(score);
-console.log(questions[3].question);
-console.log(questions[3].answer);
+console.log(startQuizButton.textContent);
